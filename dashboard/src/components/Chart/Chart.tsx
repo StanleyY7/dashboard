ad5dd0fcad3d4575b98b7abb15a1d6cba68b6e69
StanleyY7 <stanleyyu7@gmail.com>
2023-05-08 21:22:20 +1000

    updated graph with github data

--- dashboard/src/components/Chart/Chart.tsx
+++ dashboard/src/components/Chart/Chart.tsx
@@ -1,10 +1,31 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-import { data } from "../../services/data";
+
 import styles from "./Chart.module.scss";
+import { useContext } from "react";
+import { GlobalContext } from "../Context/Context";
 
 const MyChart = () => {
   ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const { github } = useContext(GlobalContext);
+
+  const data: any = {
+    labels: ["Repos", "Forked Repos"],
+    datasets: [
+      {
+        label: ["Data"],
+        data: [
+          github.length,
+          github.filter((item) => item.fork === true).length,
+        ],
+        backgroundColor: ["rgba(255,124,41,255)", "#fac797"],
+        borderColor: ["rgba(255,255,255,255))", "#f09a54"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <>
       <section>
